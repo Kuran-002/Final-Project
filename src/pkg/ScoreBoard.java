b2051078d3c85c3fd54fbd4db8cea216cd474232
luong <luong@192.168.1.212>
2025-04-27 08:31:57 -0700

    Adding score board and base game

---
+++ src/pkg/ScoreBoard.java
@@ -0,0 +1,22 @@
+package pkg;
+
+public class ScoreBoard {
+	int ones =0;
+	int twos =0;
+	int threes =0;
+	int fours =0;
+	int fives =0;
+	int twoOfaKind =0;
+	int fourOfaKind =0;
+	int fullHouse =0;
+	int smallStraight =0;
+	int latgeStraight =0;
+	int ayhtzee =0;
+	int chance =0;
+	
+	public ScoreBoard()
+	{
+		
+	}
+
+}
